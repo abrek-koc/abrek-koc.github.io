254eb71d84abaf743f291aae113b86c5134307af
Abrek Koç <44588876+abrek-koc@users.noreply.github.com>
2025-08-12 00:35:30 +0300

    test

--- assets/js/spotify-player.js
+++ assets/js/spotify-player.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
           <div class="spotify-container">
             <div class="spotify-title">
               <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABB0lEQVQ4jZXSMUoDQRTG8d+uG0ljlT5HsBASvIKFhZ1H8ASewCs4VRrBE3gDa8HGQpBgpQlYWAQMm3VmYRxmk134YJh57/3/b2bee6NVCS1McIcnvBfsueN8xSgu3uAJrxFe4xE3mMc9Ffi76QPXfuMVU9EfDPCGH5zhsJR0l3isElr4wCem2E/2LUvnh1LSSIBnBby3dH6aS/pKkm17SnCcJJ0nyZ/YwmsBnyTJmfEMOxl8mSQP+ouPM/g8SZ5oJtVW8JwNTEIVYDsBbgrJi5gdtII90/8nEh7+MvmgAXxZgWm6riN2Q4V4idf4iN1BY1X+iwE+G/J0lku96A/mImab7X+VFbw3pKpu8AsGkkRch7KzHQAAAABJRU5ErkJggg==">
-              RETRO TUNES
+              Abrek Currently Listens
             </div>
             <pre>╔═════════════════════╗
 ║     NOW PLAYING     ║
@@ -37,9 +37,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 '<div class="pause-bars"><div class="pause-bar"></div><div class="pause-bar"></div></div> ▶ NOW PLAYING' : 
                 '<div class="play-button"></div> ■ PAUSED'}
             </div>
-            <pre>╔═════════════════════╗
-║     [S]potify       ║
-╚═════════════════════╝</pre>
             <a href="${data.song_link}" class="spotify-link" target="_blank">OPEN IN SPOTIFY</a>
           </div>
         `;
--- index.markdown
+++ index.markdown
@@ -4,15 +4,3 @@
 
 layout: home
 ---
-
-<div id="spotify-status-container">
-  <div class="spotify-container">
-    <div class="spotify-title">
-      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABB0lEQVQ4jZXSMUoDQRTG8d+uG0ljlT5HsBASvIKFhZ1H8ASewCs4VRrBE3gDa8HGQpBgpQlYWAQMm3VmYRxmk134YJh57/3/b2bee6NVCS1McIcnvBfsueN8xSgu3uAJrxFe4xE3mMc9Ffi76QPXfuMVU9EfDPCGH5zhsJR0l3isElr4wCem2E/2LUvnh1LSSIBnBby3dH6aS/pKkm17SnCcJJ0nyZ/YwmsBnyTJmfEMOxl8mSQP+ouPM/g8SZ5oJtVW8JwNTEIVYDsBbgrJi5gdtII90/8nEh7+MvmgAXxZgWm6riN2Q4V4idf4iN1BY1X+iwE+G/J0lku96A/mImab7X+VFbw3pKpu8AsGkkRch7KzHQAAAABJRU5ErkJggg==">
-      MY TUNES
-    </div>
-    <div class="spotify-info">
-      <p>Loading...</p>
-    </div>
-  </div>
-</div>
